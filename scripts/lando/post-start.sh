f39ee97f7b078a4a44a91119e772c1dc3c800b4c
Karen Stevenson <karen@karen-stevenson.com>
2019-03-24 14:50:37 -0500

    Fix lando setup.

--- scripts/lando/post-start.sh
+++ scripts/lando/post-start.sh
@@ -5,9 +5,9 @@
 cd /app && composer install
 
 # Configure for phpunit tests.
-cp /app/core/phpunit.xml.dist /app/core/phpunit.xml
+cp /app/web/core/phpunit.xml.dist /app/web/core/phpunit.xml
 mkdir -p -m 777 /app/files/simpletest
 
 # Let the user know what to do next.
 echo "Run tests: lando phpunit --group [MODULE]"
-echo "To load database, run \e[1;32m lando db-import $DB_LOCAL \e[0m"
+echo "To load database: lando db-import $DB_LOCAL"
